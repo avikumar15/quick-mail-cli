ebd462cf13a952f522b779be65f18c8a7e993d95
avikumar15 <avithewinner1508@gmail.com>
2021-01-07 04:29:40 +0530

    feat: save creds at project dir :boom:

--- src/commands/init.py
+++ src/commands/init.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import pickle
 import os.path
+import shutil
 
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -9,6 +10,7 @@ from google.auth.transport.requests import Request
 from argparse import ArgumentParser, Namespace
 from zope.interface import implementer
 from src.commands import ICommand
+from src.utils.misc import heavy_tick, heavy_exclamation
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send',
           'https://www.googleapis.com/auth/gmail.readonly']
@@ -17,47 +19,68 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.send',
 @implementer(ICommand)
 class InitCommand:
     def add_arguments(self, parser: ArgumentParser) -> None:
-        pass
+        parser.add_argument('filepath',
+                            help="Path to credentials.json file")
+
+    def check_creds_json(self, path):
+
+        quick_mail_dir = os.path.expanduser('~/.quickmail')
+        quick_mail_creds_file = os.path.expanduser('~/.quickmail/credentials.json')
+
+        if os.path.exists(quick_mail_dir):
+            if os.path.exists(quick_mail_creds_file):
+                print('Credentials file already exists, skipping... ' + heavy_tick)
+                return
+            else:
+                print('Placed credentials file ' + heavy_tick)
+                shutil.copy2(path, quick_mail_creds_file)
+        else:
+            os.makedirs(quick_mail_dir)
+            shutil.copy2(path, quick_mail_creds_file)
+            print('Saved credentials file ' + heavy_tick)
 
     def run_command(self, args: Namespace):
+
+        self.check_creds_json(args.filepath)
         creds = None
+
+        token_path = os.path.expanduser('~/.quickmail/token.pickle')
+        creds_path = os.path.expanduser('~/.quickmail/credentials.json')
+
         try:
-            if os.path.exists('token.pickle'):
-                with open('token.pickle', 'rb') as token:
+            if os.path.exists(token_path):
+                with open(token_path, 'rb') as token:
                     creds = pickle.load(token)
 
             if not creds:
-                print('Have to generate token.pickle file')
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
+                    creds_path, SCOPES)
                 creds = flow.run_local_server(port=0)
 
-                with open('token.pickle', 'wb') as token:
+                with open(token_path, 'wb') as token:
                     pickle.dump(creds, token)
-                print('Generated token.pickle file')
+                print('Generated token ' + heavy_tick)
 
-            if not creds.valid or creds.expired:
-                print('Have to refresh token.pickle file')
+            elif not creds.valid or creds.expired:
                 creds.refresh(Request())
-                with open('token.pickle', 'wb') as token:
+                with open(token_path, 'wb') as token:
                     pickle.dump(creds, token)
-                print('Reinitialised token file')
+                print('Initialised token ' + heavy_tick)
 
             else:
-                print('Token file already exists, skipping...')
+                print('Initialised token ' + heavy_tick)
 
         except pickle.UnpicklingError:
 
-            if os.path.exists('token.pickle'):
-                os.remove('token.pickle')
+            if os.path.exists(token_path):
+                os.remove(token_path)
 
-            print('token.pickle file is corrupted.')
-            print('Have to generate token file')
+            print('Token file corrupted ' + heavy_exclamation + ' regenerating...')
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                creds_path, SCOPES)
             creds = flow.run_local_server(port=0)
 
-            with open('token.pickle', 'wb') as token:
+            with open(token_path, 'wb') as token:
                 pickle.dump(creds, token)
 
-            print('Generated token file')
+            print('Initialised token ' + heavy_tick)
