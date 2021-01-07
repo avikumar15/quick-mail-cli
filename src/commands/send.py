6e6d153c43f30be00ade1366ac6d7d7153d9759b
avikumar15 <avithewinner1508@gmail.com>
2021-01-08 04:49:07 +0530

    feat: let user type body in nano window :tada: :tada:

--- src/commands/send.py
+++ src/commands/send.py
@@ -14,6 +14,10 @@ from googleapiclient.discovery import build
 from argparse import ArgumentParser, Namespace
 from zope.interface import implementer
 from src.commands import ICommand
+from src.utils.misc import quick_mail_dir, quick_mail_token_file, wink_face, grinning_face, heavy_tick, smiling_face, \
+    party_popper_tada
+from datetime import datetime
+import subprocess
 
 
 @implementer(ICommand)
@@ -21,32 +25,51 @@ class SendCommand:
     def add_arguments(self, parser: ArgumentParser) -> None:
         parser.add_argument('--receiver', '-r',
                             required=True,
-                            help="Receiver's email address, eg. 'avithewinner1508@gmail.com'")
+                            help="Receiver's email address, eg. '-r \"avithewinner1508@gmail.com\"' ")
         parser.add_argument('--subject', '-sub',
                             required=True,
-                            help="Email's subject, eg. 'CA Endsem Submission [106118017]'")
+                            help="Email's subject, eg. '-sub \"CA Endsem Submission'\"")
         parser.add_argument('--body', '-b',
-                            required=True,
-                            help="Email's body file path, eg. '~/Desktop/body.txt' ")
+                            help="Email's body file path, eg. '-b ~/Desktop/body.txt' or '-b \"Message Body Comes "
+                                 "Here\"' ")
         parser.add_argument('--attachment', '-a',
-                            help="Email's attachment path, eg. '~/Desktop/106118017_CA_Endsem.pdf' ")
+                            help="Email's attachment path, eg. '~/Desktop/CA_Endsem.pdf' ")
 
     def run_command(self, args: Namespace):
+
+        creds = None
+
+        # if token.pickle file is missing, init command should be run
+        if os.path.exists(quick_mail_token_file):
+            with open(quick_mail_token_file, 'rb') as token:
+                creds = pickle.load(token)
+        else:
+            print('Run init command before send')
+            exit(0)
 
         receiver_email = args.receiver
         subject = args.subject
         body = args.body
         attachment = args.attachment
 
-        print('\nPreparing to send mail')
+        if not body:
+            body_file_name = '/' + datetime.now().strftime("%d_%m_%Y_%H_%M_%S") + '.txt'
+            file_path = quick_mail_dir+body_file_name
 
-        # if token.pickle file is missing, init command should be run
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
+            f = open(file_path, "x")
+            # print(file_path)
+            subprocess.call(['nano', file_path])
+            f.close()
+            f = open(file_path, "r")
+            body = f.read()
+            # print(body)
+            print('\nAdded body of the mail ' + heavy_tick)
+            f.close()
+        # Hacky fix to add a new line if body is not null
         else:
-            print('Run init command before send')
-            exit(0)
+            print()
+
+        print('Preparing to send mail ' + grinning_face)
 
         # build service
         service = build('gmail', 'v1', credentials=creds)
@@ -56,12 +79,12 @@ class SendCommand:
 
         # Show user mail summary
         print('\nFrom: ' + senders_email + '\nTo: '+receiver_email + '\nSubject: ' + subject + '\nBody\n' + body +
-              '\nAttachment Path: ' + str(attachment) + '\n')
+              '\nAttachment Path: ' + (str(attachment) if attachment else 'No attachments') + '\n')
 
         is_confirm = str(input('Confirm send? (Y/N): '))
 
         if is_confirm.lower() != 'y':
-            print('Confirmation denied, exiting...')
+            print('Confirmation denied, exiting... ' + smiling_face + smiling_face + smiling_face)
             exit(0)
 
         if not attachment:
@@ -117,7 +140,7 @@ class SendCommand:
         try:
             message = (service.users().messages().send(userId=senders_email, body=message)
                        .execute())
-            print(message)
+            print('Mail delivered ' + party_popper_tada + party_popper_tada + party_popper_tada)
         except BaseException as e:
             print('Could not send message: ', e)
 
