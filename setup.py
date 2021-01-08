4ab24c839bc2f385b263ebc6086acee550e5a18e
avikumar15 <avithewinner1508@gmail.com>
2021-01-09 00:47:40 +0530

    add: new fields in setup.py

--- setup.py
+++ setup.py
@@ -11,15 +11,16 @@ with open(os.path.join(current_path, 'requirements.txt'), encoding='utf-8') as f
 # print(install_requires)
 
 setup(
-    name='quickmailcli',
+    name='quickmail',
     description='A simple commandline application for sending mails quickly',
     version='1.0.0',
     install_requires=install_requires,
-    author='Avi Kumar',
+    author='Avi Kumar Singh',
     author_email='avikumar.singh1508@gmail.com',
     python_requires='>=3.6',
     package_dir={'': 'src'},
     entry_points={'console_scripts': ['quickmailcli=src.quickmailcli.cli:execute']},
     url='https://github.com/avikumar15/quick-email-cli',
-    keywords=['CLI', 'gmail', 'email']
+    keywords=['CLI', 'gmail', 'email'],
+    platforms='any',
 )
